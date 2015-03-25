17ce59f548cc03aa8ad63fa130c6e62da19a0f70
julien-monniot <n.ceska@gmail.com>
2015-03-25 10:00:18 +0100

    Client class created + connection of client to tun interface (tun2) compile and connect (it seems)

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -7,8 +7,23 @@
 //
 // ---------------------------------------------
 
+// Inlude project
+#include "client.h"
+
+// include system
+#include <iostream>
+#include <string>
+
 int main( int argc, const char* argv[] )
 {
+    std::string iname = "tun2";
+
+    std::cout << "Client creation..."<< std::endl;    
+    Client clt(iname, (IFF_TUN | IFF_NO_PI));
+    clt.ConnectInterface();
+    int fd = clt.getFd();
+    std::cout << "Client creation [DONE] - File desc = " << fd << std::endl;
+
     return 0;
 }
 
