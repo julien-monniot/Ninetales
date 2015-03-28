82a4a1ef8ab54c315766d5ee993dbb0fee75a024
julien-monniot <n.ceska@gmail.com>
2015-03-28 18:22:42 +0100

    Server launched using launcher.py

--- python/server.py
+++ python/server.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python3
-
 ##########################################
 #                                        #
-#   Script Server for pppd/stunnel VPN   #
+#   Server for pppd/stunnel VPN          #
 #   created : 28/03/15                   #
 #   author : Julien M                    #
 #                                        #
@@ -11,121 +9,111 @@
 import os
 import re
 
-# CST
+class Server:
 
-IP_FORWARD_PATH = "/proc/sys/net/ipv4/ip_forward"
-VPN_FOLDER = "Ninetale"
-VPN_USER = "sslvpn"
-VPN_USER_PATH = "/opt/ssl-vpn"
+	def __init__(self, vpn_name, vpn_user, vpn_user_path):
+		# Member variable
+		self.vpn_name = vpn_name
+		self.vpn_user = vpn_user
+		self.vpn_user_path = vpn_user_path
 
-# Functions
+	def Initialize(self):
+		# ppp, stunnel, route installed ?
+		print("Packet checking...")
+		packetList = checkPackets()
+		if len(packetList) == 0 :
+			print("     OK - No missing packet.")
+		elif packetList[0] == "OS_UNKNOWN" :
+			print("     Unable to check for packets due to unsupported OS.\n Script will continue anyway but might fail.")
+		else:
+			print("     Missing packets :")
+			[print("\t\t"+x+"\n") for x in packetList]
+			return False # Missing packets, impossible to continue
 
-# Check packet requirements :
-# returns a list :
-# - of missing packets (so an empty list means everything is ok)
-# - of a single item stating that os is not supported and the
-#   script can't find which packets are installed
-def checkPackets():
-    # Regex check on the system :
-    result = re.search(r".*(arch).*", os.popen('uname -r').read(), re.IGNORECASE)
-    missPackets = []
-    # Finding packets :
-    if (result):
-        print("Arch system  matched - checking required packets.")
-        if (os.system('pacman -Qs ppp >/dev/null')) == 1:
-            #print("PPP not installed")
-            missPackets.append("ppp")
-        if (os.system('pacman -Qs stunnel >/dev/null')) == 1:
-            #print("sTunnel not installed")
-            missPackets.append("stunnel")
-        if (os.system('pacman -Qs net-tools >/dev/null')) == 1:
-            #print("net-tools not installed")
-            missPackets.append("net-tools")
-        ## TEST
-#        if (os.system('pacman -Qs bidon')):
-#           missPackets.append("bidon")
-        ## TEST
-        return missPackets
-    else:
-        print("Not an Arch - not able to check automatically for packets")
-        missPackets.append("OS_UNKNOWN")
-        return missPackets
+		# Activate ip forwarding :
+		print("Ip forwarding...")
+		if(checkIpForward()):
+			print("     Ip forwarding activated.")
+		else:
+			print("     Ip forwarding could not be set.")
+			return False     # Ip forwarding could not be set
 
-# check ip forwarding
-def checkIpForward():
-    print("Activating ip forwarding...")
-    result = re.search(r".*1$", os.popen("sysctl net.ipv4.ip_forward=1").read())
-    if (result):
-        return True
-    else:
-        return False
+	    # Is user/group sslvpn present ?
+	    print("Checking user "+self.vpn_user+"...")
+	    if checkForUser(self.vpn_user):
+			print("     User "+self.vpn_user+" found.")
+	    else:
+			print("     User "+self.vpn_user+" not found")
+			print("     Group and user creation")
+			if useradd():
+			    print("User Created")
+			else:
+		    	print("Wasn't able to create user or group.")
+		    	return False # Group/user creation failed
 
-# Check for a specified user on the system
-def checkForUser(username):
-    result = re.search(r".*"+username+":.*", os.popen('less /etc/passwd').read())
-    if (result):
-        return True
-    else:
-        return False
+	    #   If we've got so far, init is ok
+	    return True
 
 
-# Add user with specified username and homepath
-# Note : will prompt for user password as sudo is used, unless visudo is configured
-def useradd(username, homepath):
-    grCmd = "groupadd "+username
-    usrCmd = "useradd -m -d "+homepath+" -c 'SSL VPN User' -g "+username+" "+username
-    ret1 = os.system(grCmd)
-    ret2 = os.system(usrCmd)
-    if ( ret1 == 0 and ret2 == 0 ):
-        folderCmd1 = "sudo -u sslvpn mkdir "+VPN_USER_PATH+"/etc"
-        folderCmd2 = "sudo -u sslvpn mkdir "+VPN_USER_PATH+"/etc/"+VPN_FOLDER
-        if (str(os.popen(folderCmd1)) + str(os.popen(folderCmd2)) == ""):
-            return True
-    else :
-        return False
+	# Class Methods :
 
-def keyAndCertGen(path):
-    pass
+	# Check packet requirements :
+	# returns a list :
+	# - of missing packets (so an empty list means everything is ok)
+	# - of a single item stating that os is not supported and the
+	#   script can't find which packets are installed
+	def checkPackets():
+		# Regex check on the system :
+		result = re.search(r".*(arch).*", os.popen('uname -r').read(), re.IGNORECASE)
+		missPackets = []
+		# Finding packets :
+		if (result):
+			print("Arch system  matched - checking required packets.")
+			if (os.system('pacman -Qs ppp >/dev/null')) == 1:
+				#print("PPP not installed")
+				missPackets.append("ppp")
+			if (os.system('pacman -Qs stunnel >/dev/null')) == 1:
+				#print("sTunnel not installed")
+				missPackets.append("stunnel")
+			if (os.system('pacman -Qs net-tools >/dev/null')) == 1:
+				#print("net-tools not installed")
+				missPackets.append("net-tools")
+				return missPackets
+		else:
+			print("Not an Arch - not able to check automatically for packets")
+			missPackets.append("OS_UNKNOWN")
+			return missPackets
 
+	# check ip forwarding
+	def checkIpForward():
+	    print("Activating ip forwarding...")
+	    result = re.search(r".*1$", os.popen("sysctl net.ipv4.ip_forward=1").read())
+	    if (result):
+			return True
+	    else:
+			return False
 
-#----------------------------------------#
-# - - - - - - - -  SCRIPT - - - - - - - -#
-#----------------------------------------#
+	# Check for a specified user on the system
+	def checkForUser(self):
+	    result = re.search(r".*"+self.username+":.*", os.popen('less /etc/passwd').read())
+	    if (result):
+			return True
+	    else:
+			return False
 
-
-# ppp, stunnel, route installed ?
-print("Packet checking...")
-packetList = checkPackets()
-if len(packetList) == 0 :
-    print("OK - No missing packet")
-elif packetList[0] == "OS_UNKNOWN" :
-    print("Unable to check for packets due to unsupported OS.\n Script will continue anyway but might fail")
-else:
-    print("Missing packets :")
-    [print(x+"\n") for x in packetList]
-    exit(1)
-
-# Activate ip forwarding :
-if(checkIpForward()):
-    print("Ip forwarding activated")
-else:
-    print("Ip forwarding failed")
-    exit(1)
-
-# Is user/group sslvpn present ?
-print("User checking...")
-if checkForUser(VPN_USER):
-    print("User "+VPN_USER+" found.")
-else:
-    print("Group and user creation")
-    if useradd(VPN_USER, VPN_USER_PATH):
-        print("User Created")
-    else:
-        print("Script wasn't able to create user or group - Exiting now")
-        exit(1)
-
-# Open SSL Call for key/cert
-
-
-
+	# Add user with specified username and homepath
+	# Note : will prompt for user password as sudo is used, unless visudo is configured
+	def addUser(self):
+	    grCmd = "groupadd "+self.vpn_user
+	    usrCmd = "useradd -m -d "+self.vpn_user_path+" -c 'SSL VPN User' -g "+self.vpn_user+" "+self.vpn_user
+	    ret1 = os.system(grCmd)
+	    ret2 = os.system(usrCmd)
+	    if ( ret1 == 0 and ret2 == 0 ):	
+        	folderCmd1 = "sudo -u sslvpn mkdir "+self.vpn_user_path+"/etc"
+        	folderCmd2 = "sudo -u sslvpn mkdir "+self.vpn_user_path+"/etc/"+self.vpn_name
+        	if (os.popen(folderCmd1).read() + os.popen(folderCmd2).read() == ""):
+            	return True	# mkdir failed
+    		else:
+        		return False
+		return False;	# User/group add failed
 
